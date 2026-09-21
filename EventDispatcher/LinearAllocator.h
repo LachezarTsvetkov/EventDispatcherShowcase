73e8e12a0c28a00796dc476d4a9a7dc63f2ef9a2
Lachezar <lachezardts@gmail.com>
2026-09-21 09:52:55 +0300

    Added a destructor, copy constructor, and an assignment operator to the LinearAllocator

--- EventDispatcher/LinearAllocator.h
+++ EventDispatcher/LinearAllocator.h
@@ -9,6 +9,16 @@ class LinearAllocator
 	public:
 	LinearAllocator(size_t size) : m_capacity(size), m_Offset(0) {}
 
+	~LinearAllocator()
+	{
+		delete[] m_Buffer;
+		m_Buffer = nullptr;
+	}
+
+	// Delete copy constructor and assignment operator to prevent copying
+	LinearAllocator(const LinearAllocator&) = delete;
+	LinearAllocator& operator=(const LinearAllocator&) = delete;
+
 	void* Allocate(size_t size)
 	{
 		assert(m_Offset + size <= m_capacity && "Allocation exceeds buffer size!");
@@ -17,6 +27,7 @@ class LinearAllocator
 		return ptr;
 	}
 
+	// This should be called at the end of a frame or when you want to reset the allocator for reuse.
 	void Reset()
 	{
 		m_Offset = 0;
@@ -26,7 +37,6 @@ class LinearAllocator
 	size_t GetUsedMemory() const { return m_Offset; }
 private:
 	uint8_t* m_Buffer;
-	size_t m_capacity;
-	size_t m_Offset;
+	size_t m_capacity, m_Offset;
 };
 
